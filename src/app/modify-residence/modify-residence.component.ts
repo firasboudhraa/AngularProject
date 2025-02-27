6fff720dc18dcc682dec79161fb870f744c7aa9b
firasboudhraa <fboudhraa2@gmail.com>
2025-02-27 16:48:32 +0100

    modifying is done using the service and json server

--- src/app/modify-residence/modify-residence.component.ts
+++ src/app/modify-residence/modify-residence.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import  { Residence } from '../core/models/residence';
 import  { ActivatedRoute, Router } from '@angular/router';
 import  { ResidenceService } from '../services/residence.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modify-residence',
@@ -11,15 +12,40 @@ import  { ResidenceService } from '../services/residence.service';
 export class ModifyResidenceComponent {
  residence!: Residence
  id!: number
+ ResidenceForm!: FormGroup;
+ statusOptions: string[] = ["Disponible", "En construction", "Vendu"];
+ 
 
  constructor(
   private Act:ActivatedRoute,
   private mr:ResidenceService,
   private router:Router
- ){}
-
- ngOnInit(){
-  this.id= this.Act.snapshot.params['id']
+ ){
+  this.ResidenceForm = new FormGroup({
+    id: new FormControl('', [Validators.required, Validators.min(1)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    address: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    status: new FormControl('', [Validators.required]),
+    image: new FormControl('',Validators.required)
+  });
  }
+
+   ngOnInit() {
+    this.id=this.Act.snapshot.params['id']
+    this.mr.getResidenceById(this.id).subscribe(
+      (data) =>this.ResidenceForm.patchValue(data)
+    )
+   }
+
+   onSubmit(){
+    if (this.ResidenceForm.valid) {      
+      this.mr.updateResidence(this.ResidenceForm.value).subscribe({
+        next: () => {
+          console.log('Résidence ajoutée avec succès !');
+          this.router.navigate(['/residences']); 
+        },
+      });
+    }
+   }
 
 }
